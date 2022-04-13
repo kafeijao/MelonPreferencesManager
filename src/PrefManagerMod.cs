c6478c53a8ed7ed4b11632760a2c08c419380c2b
Sinai <49360850+sinai-dev@users.noreply.github.com>
2022-04-14 02:31:43 +1000

    bump version

--- src/PrefManagerMod.cs
+++ src/PrefManagerMod.cs
@@ -26,7 +26,7 @@ namespace MelonPrefManager
         public const string GUID = "com.sinai.MelonPreferencesManager";
         public const string NAME = "MelonPreferencesManager";
         public const string AUTHOR = "Sinai";
-        public const string VERSION = "1.0.12";
+        public const string VERSION = "1.2.0";
 
         public static PrefManagerMod Instance { get; private set; }
 
