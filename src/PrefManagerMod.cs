67966254b0981561f9f271043f5b9d09fc1f8c0e
Sinai <49360850+sinai-dev@users.noreply.github.com>
2022-04-24 05:59:54 +1000

    Bump UniverseLib

--- src/PrefManagerMod.cs
+++ src/PrefManagerMod.cs
@@ -26,7 +26,7 @@ namespace MelonPrefManager
         public const string GUID = "com.sinai.MelonPreferencesManager";
         public const string NAME = "MelonPreferencesManager";
         public const string AUTHOR = "Sinai";
-        public const string VERSION = "1.2.5";
+        public const string VERSION = "1.2.7";
 
         public static PrefManagerMod Instance { get; private set; }
 
